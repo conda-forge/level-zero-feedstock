39dd3a1a7833f92146b2e2214ca223b57814b3d8
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-04-10 11:00:03 -0500

    Use cp -a to preserve symbolic links

--- recipe/move-level-zero-devel.sh
+++ recipe/move-level-zero-devel.sh
@@ -5,9 +5,9 @@ set -ex
 pushd ${SRC_DIR}
 mkdir -p ${PREFIX}/lib/pkgconfig
 mkdir -p ${PREFIX}/include
-cp -r install/include/* ${PREFIX}/include/
-cp -r install/lib*/pkgconfig/* ${PREFIX}/lib/pkgconfig
-cp install/lib*/libze_loader.so ${PREFIX}/lib/
-cp install/lib*/libze_tracing_layer.so ${PREFIX}/lib/
-cp install/lib*/libze_validation_layer.so ${PREFIX}/lib/
+cp -a install/include/* ${PREFIX}/include/
+cp -a install/lib*/pkgconfig/* ${PREFIX}/lib/pkgconfig
+cp -a install/lib*/libze_loader.so ${PREFIX}/lib/
+cp -a install/lib*/libze_tracing_layer.so ${PREFIX}/lib/
+cp -a install/lib*/libze_validation_layer.so ${PREFIX}/lib/
 popd
--- recipe/move-level-zero.sh
+++ recipe/move-level-zero.sh
@@ -4,7 +4,7 @@ set -ex
 
 pushd ${SRC_DIR}
 mkdir -p ${PREFIX}/lib
-cp install/lib*/libze_loader.so.1* ${PREFIX}/lib/
-cp install/lib*/libze_tracing_layer.so.1* ${PREFIX}/lib/
-cp install/lib*/libze_validation_layer.so.1* ${PREFIX}/lib/
+cp -a install/lib*/libze_loader.so.1* ${PREFIX}/lib/
+cp -a install/lib*/libze_tracing_layer.so.1* ${PREFIX}/lib/
+cp -a install/lib*/libze_validation_layer.so.1* ${PREFIX}/lib/
 popd
