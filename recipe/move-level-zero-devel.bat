9b646a8ed88197d7d6b6f764482a093c3bf2d6b3
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-08 14:45:24 -0500

    Fixed error checking in batch scripts

--- recipe/move-level-zero-devel.bat
+++ recipe/move-level-zero-devel.bat
@@ -4,11 +4,11 @@ robocopy /E ".\install\include\" "%LIBRARY_INC%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
 copy /Y ".\install\lib\ze_loader.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% exit 1
+if %ERRORLEVEL% 1 exit 1
 
 copy /Y ".\install\lib\ze_tracing_layer.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% exit 1
+if %ERRORLEVEL% 1 exit 1
 
 copy /Y ".\install\lib\ze_validation_layer.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% exit 1
+if %ERRORLEVEL% 1 exit 1
 
--- recipe/move-level-zero.bat
+++ recipe/move-level-zero.bat
@@ -1,13 +1,13 @@
 @echo on
 
-if not exist "%PREFIX%/lib" mkdir "%PREFIX%/lib"
+dir /S install
 
 copy /Y ".\install\bin\ze_loader.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% exit 1
+if errorlevel 1 exit 1
 
 copy /Y ".\install\bin\ze_tracing_layer.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% exit 1
+if errorlevel 1 exit 1
 
-robocopy /E ".\install\bin\ze_validation_layer.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% exit 1
+copy /Y ".\install\bin\ze_validation_layer.dll" "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
 
