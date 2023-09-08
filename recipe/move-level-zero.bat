7777fb1fef7eb816b760f5b6eb060874c666637a
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-08 13:57:57 -0500

    Replaced robocopy with copy for single file copying

--- recipe/move-level-zero.bat
+++ recipe/move-level-zero.bat
@@ -2,12 +2,12 @@
 
 if not exist "%PREFIX%/lib" mkdir "%PREFIX%/lib"
 
-robocopy /E ".\install\bin\ze_loader.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% GEQ 8 exit 1
+copy /Y ".\install\bin\ze_loader.dll" "%LIBRARY_BIN%"
+if %ERRORLEVEL% exit 1
 
-robocopy /E ".\install\bin\ze_tracing_layer.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% GEQ 8 exit 1
+copy /Y ".\install\bin\ze_tracing_layer.dll" "%LIBRARY_BIN%"
+if %ERRORLEVEL% exit 1
 
 robocopy /E ".\install\bin\ze_validation_layer.dll" "%LIBRARY_BIN%"
-if %ERRORLEVEL% GEQ 8 exit 1
+if %ERRORLEVEL% exit 1
 
