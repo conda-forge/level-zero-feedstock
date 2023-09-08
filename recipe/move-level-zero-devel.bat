b0b871959a377361b1d777c3ec400caabe12b159
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-08 16:08:08 -0500

    Skip layers libraries on Windows until level-zero CMake script gets fixed
    See https://github.com/oneapi-src/level-zero/pull/122

--- recipe/move-level-zero-devel.bat
+++ recipe/move-level-zero-devel.bat
@@ -1,14 +1,19 @@
 @echo on
 
-robocopy /E ".\install\include\" "%LIBRARY_INC%"
+robocopy ".\install\include" "%LIBRARY_INC%" /S /E
 if %ERRORLEVEL% GEQ 8 exit 1
 
 copy /Y ".\install\lib\ze_loader.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% 1 exit 1
+if errorlevel 1 exit 1
 
-copy /Y ".\install\lib\ze_tracing_layer.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% 1 exit 1
+REM LAYER DLL files are not correctly installed by Level-Zero
+REM CMakeLists.txt. They are skipped on Windows for now, and hence 
+REM LIB files are not copied as well. See
+REM    https://github.com/oneapi-src/level-zero/pull/122
 
-copy /Y ".\install\lib\ze_validation_layer.lib" "%LIBRARY_LIB%"
-if %ERRORLEVEL% 1 exit 1
+REM copy /Y ".\install\lib\ze_tracing_layer.lib" "%LIBRARY_LIB%"
+REM if %ERRORLEVEL% 1 exit 1
+
+REM copy /Y ".\install\lib\ze_validation_layer.lib" "%LIBRARY_LIB%"
+REM if %ERRORLEVEL% 1 exit 1
 
