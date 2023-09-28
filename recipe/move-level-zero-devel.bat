cb2c79e1f36030ebcf8a7335c33f56bd28b129d8
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-09-28 13:28:28 -0500

    The move-level-zero-devel must create %LIBRARY_LIB% before copying files
    Also added test steps to ensure that files had been copied.

--- recipe/move-level-zero-devel.bat
+++ recipe/move-level-zero-devel.bat
@@ -3,8 +3,12 @@
 robocopy ".\install\include" "%LIBRARY_INC%" /S /E
 if %ERRORLEVEL% GEQ 8 exit 1
 
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
+
 copy /Y ".\install\lib\ze_loader.lib" "%LIBRARY_LIB%"
 if errorlevel 1 exit 1
+
+if not exist "%LIBRARY_LIB%\ze_loader.lib" exit 1
 
 REM LAYER DLL files are not correctly installed by Level-Zero
 REM CMakeLists.txt. They are skipped on Windows for now, and hence 
