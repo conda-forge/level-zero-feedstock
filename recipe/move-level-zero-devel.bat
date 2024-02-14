2dd4a4de6c621fc789f76302dc7a338d0f3235cb
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-02-14 13:34:30 -0600

    Include layer files on Windows as the bug was fixed upstream
    The upstream PR
       https://github.com/oneapi-src/level-zero/pull/122 has been merged
    has been merged.

--- recipe/move-level-zero-devel.bat
+++ recipe/move-level-zero-devel.bat
@@ -10,14 +10,9 @@ if errorlevel 1 exit 1
 
 if not exist "%LIBRARY_LIB%\ze_loader.lib" exit 1
 
-REM LAYER DLL files are not correctly installed by Level-Zero
-REM CMakeLists.txt. They are skipped on Windows for now, and hence 
-REM LIB files are not copied as well. See
-REM    https://github.com/oneapi-src/level-zero/pull/122
+copy /Y ".\install\lib\ze_tracing_layer.lib" "%LIBRARY_LIB%"
+if %ERRORLEVEL% 1 exit 1
 
-REM copy /Y ".\install\lib\ze_tracing_layer.lib" "%LIBRARY_LIB%"
-REM if %ERRORLEVEL% 1 exit 1
-
-REM copy /Y ".\install\lib\ze_validation_layer.lib" "%LIBRARY_LIB%"
-REM if %ERRORLEVEL% 1 exit 1
+copy /Y ".\install\lib\ze_validation_layer.lib" "%LIBRARY_LIB%"
+if %ERRORLEVEL% 1 exit 1
 
--- recipe/move-level-zero.bat
+++ recipe/move-level-zero.bat
@@ -7,13 +7,9 @@ if errorlevel 1 exit 1
 
 if not exist "%LIBRARY_BIN%\ze_loader.dll" exit 1
 
-REM LAYER DLL files are not correctly installed by Level-Zero
-REM CMakeLists.txt. They are skipped on Windows for now. See
-REM    https://github.com/oneapi-src/level-zero/pull/122
+copy /Y ".\install\bin\ze_tracing_layer.dll" "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
 
-REM copy /Y ".\install\bin\ze_tracing_layer.dll" "%LIBRARY_BIN%"
-REM if errorlevel 1 exit 1
-
-REM copy /Y ".\install\bin\ze_validation_layer.dll" "%LIBRARY_BIN%"
-REM if errorlevel 1 exit 1
+copy /Y ".\install\bin\ze_validation_layer.dll" "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
 
